3c2873909e17eeddd1a9374c694d2394bc35ef5a
khangpv <vkphambn@gmail.com>
2016-11-26 06:36:19 +0700

    123123

--- ggmap.js
+++ ggmap.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {MapView} from 'react-native';
+import {MapView, TextInput} from 'react-native';
 import css from './style'
 export default class MapMyRide extends Component {
     constructor() {
@@ -28,60 +28,7 @@ export default class MapMyRide extends Component {
 
                 region={this.state.region}
             />
+            <TextInput style={{height: 40, borderColor: 'gray', borderWidth: 1}} onChangeText={(text) => this.setState({text})} value={this.state.text} />
         );
-    }
-}
-
-const earthRadiusInKM = 6371;
-// you can customize these two values based on your needs
-const radiusInKM = 1;
-const aspectRatio = 1;
-
-class Map extends Component {
-    constructor(props) {
-        super(props);
-
-        // this will be the map's initial region
-        this.state = {
-            region: {
-
-                longitude: 21.0064807,
-                latitude: 105.799677,
-            }
-        };
-    }
-
-    // you need to invoke this method to update your map's region.
-    showRegion(locationCoords) {
-        if (locationCoords && locationCoords.latitude && locationCoords.longitude) {
-            var radiusInRad = radiusInKM / earthRadiusInKM;
-            var longitudeDelta = this.rad2deg(radiusInRad / Math.cos(this.deg2rad(locationCoords.latitude)));
-            var latitudeDelta = aspectRatio * this.rad2deg(radiusInRad);
-
-            this.setState({
-                region: {
-                    latitude: locationCoords.latitude,
-                    longitude: locationCoords.longitude,
-                    latitudeDelta: latitudeDelta,
-                    longitudeDelta: longitudeDelta
-                }
-            });
-        }
-    }
-
-    render() {
-        return (
-            <MapView
-                style={{flex: 1}}
-                region={this.state.region}/>
-        )
-    }
-
-    deg2rad(angle) {
-        return angle * 0.017453292519943295 // (angle / 180) * Math.PI;
-    }
-
-    rad2deg(angle) {
-        return angle * 57.29577951308232 // angle / Math.PI * 180
     }
 }
